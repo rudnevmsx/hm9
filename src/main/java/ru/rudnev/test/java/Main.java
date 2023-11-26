2563aac9f458b492569f744a5cbedf96e32e2957
rudnevmsx <rudnevmsx@gmail.com>
2023-11-26 20:07:22 +0300

    hm done!

--- src/main/java/ru/rudnev/test/java/Main.java
+++ src/main/java/ru/rudnev/test/java/Main.java
@@ -1,7 +1,12 @@
 package ru.rudnev.test.java;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("HelloWorld!");
     }
+
 }
