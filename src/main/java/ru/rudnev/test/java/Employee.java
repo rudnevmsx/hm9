5a049bf60516bd3732bfad1dccc00ad3d08e7b31
rudnevmsx <rudnevmsx@gmail.com>
2023-11-27 20:00:04 +0300

    hm done

---
+++ src/main/java/ru/rudnev/test/java/Employee.java
@@ -0,0 +1,90 @@
+package ru.rudnev.test.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    /***
+     *
+     * @param list
+     * @return
+     * Реализуйте метод, принимающий в качестве аргумента список сотрудников,
+     * и возвращающий список их имен;
+     */
+    public static List<String> getEmloyeesName(List<Employee> list){
+        List<String> names = new ArrayList<>();
+        for (Employee employee : list){
+            names.add(employee.getName());
+        }
+        return names;
+    }
+
+    /***
+     *
+     * @param list
+     * @param value
+     * @return
+     * Реализуйте метод, принимающий в качестве аргумента список сотрудников и минимальный возраст,
+     * и возвращающий список сотрудников, возраст которых больше либо равен указанному аргументу;
+     */
+    public static List<String> getMinAge(List<Employee> list, int value){
+        List<String> emp = new ArrayList<>();
+        for (Employee employee : list){
+            if (employee.getAge() >= value){
+                emp.add(employee.getName());
+            }
+        }
+        return emp;
+    }
+
+    /***
+     * @param list
+     * @param reqAge
+     * @return
+     * Реализуйте метод, принимающий в качестве аргумента список сотрудников и минимальный средний возраст
+     * и проверяющий что средний возраст сотрудников превышает указанный аргумент;
+     */
+    public static boolean checkAverageAge(List<Employee> list, int reqAge){
+        List<Integer> ages = new ArrayList<>();
+        int sum = 0;
+        for (Employee employee : list){
+            ages.add(employee.getAge());
+        }
+        for (int i = 0; i < ages.size(); i++) {
+            sum += ages.get(i);
+        }
+        if (sum / ages.size() == reqAge){
+            return true;
+        }
+        return false;
+    }
+
+    public static Employee getYoungest(List<Employee> employees){
+        Employee youngest = employees.get(0);
+        for (Employee employee : employees) {
+            if (employee.getAge() < youngest.getAge()) {
+                youngest = employee;
+            }
+        }
+        return youngest;
+    }
+
+
+
+}
